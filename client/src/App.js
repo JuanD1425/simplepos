754c090facd4e7994213e03727913e6fdfa8f41f
Juan Diego Gutierrez <JuanD1425@me.com>
2019-11-17 18:16:32 -0500

    need to fix mysql

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import './untitled.css'
-import PageNotFound from "./components/PageNotFound";
+// import PageNotFound from "./components/PageNotFound";
+import Inventory from "./components/Inventory";
 import Registration from "./components/Registration/RegistrationPage";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function Index() {
-  return (
-    // <div className="Index">
-    //   <Ind />
-    // </div>
+class Index extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
 
+callAPI() {
+    fetch("http://localhost:3001/express_backend")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+  
+  render(){
+  return (
     <Router>
-        <div>
-          <Route path="/" exact strict render={
-            () => {
-              return <Login />;
-            }
-          }/>
-          <Route path="/registration" exact strict render={
-            () => {
-              return <Registration />;
-            }
-          }/>
-          <Route path="/profile" exact strict render={() => {return <Profile />;}}/>
-          <Route exact strict render={
+      <div>
+        <Route
+          path="/"
+          exact
+          strict
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          path="/inventory"
+          exact
+          strict
+          render={() => {
+            return <Inventory />;
+          }}
+        />
+        <Route
+          path="/registration"
+          exact
+          strict
+          render={() => {
+            return <Registration />;
+          }}
+        />
+        <Route
+          path="/profile"
+          exact
+          strict
+          render={() => {
+            return <Profile />;
+          }}
+        />
+        {/* <Route exact strict render={
             () => {
               return <PageNotFound />;
             }
-          }/>
-        </div>
+          }/> */}
+      </div>
     </Router>
   );
+}
 }
 
 export default Index;
--- server/app.js
+++ server/app.js
@@ -1,54 +1,77 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var db = require("./models");
+var cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require("./routes/html-routes");
+var userRouter = require("./routes/users");
+
+var app = express();
+const PORT = 3001;
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME
+});
+
+connection.connect(function(err) {
+  err
+    ? console.log(err + "+++++++++++++++//////////")
+    : console.log("connection********");
+});
+
 require("./routes/html-routes")(app, connection);
 
-const app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
+app.use("/", indexRouter);
+app.use("/users", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-//route for insert data
-// app.post('/',(req, res) => {
-//   let data = {name: {"firstName": req.body.firstName, "lastName": req.body.lastName}, email: req.body.email, businessName: req.body.businessName, businessAddress: {"streetAddress": req.body.businessAddress, "city": req.body.city, "state": req.body.state, "zip": req.body.zip}, password: req.body.password};
-//   let sql = "INSERT INTO user SET ?";
-//   let query = connection.query(sql, data,(err, results) => {
-//     if(err) throw err;
-//     res.redirect('/');
-//   });
-// });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
 
 module.exports = app;
